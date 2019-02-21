68ac70751a65d041913ce881bf34541055a0ec4a
Mikhail <wowivan2011@yandex.ru>
2019-02-21 15:39:01 +0300

    support win32

--- Source/DigitFfmpeg/DigitFfmpeg.Build.cs
+++ Source/DigitFfmpeg/DigitFfmpeg.Build.cs
@@ -9,51 +9,91 @@ public class DigitFfmpeg : ModuleRules
     {
         get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "ThirdParty", "ffmpeg")); }
     }
-	public DigitFfmpeg(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
+
+    private string UProjectPath
+    {
+        get { return Directory.GetParent(ModuleDirectory).Parent.FullName; }
+    }
+
+    private void CopyToBinaries(string Filepath, ReadOnlyTargetRules Target)
+    {
+        string binariesDir = Path.Combine(UProjectPath, "Binaries", Target.Platform.ToString());
+        string filename = Path.GetFileName(Filepath);
+
+        //System.Console.WriteLine("Writing file " + Filepath + " to " + binariesDir);
+
+        if (!Directory.Exists(binariesDir))
+            Directory.CreateDirectory(binariesDir);
+
+        if (!File.Exists(Path.Combine(binariesDir, filename)))
+            File.Copy(Filepath, Path.Combine(binariesDir, filename), true);
+    }
+
+    public DigitFfmpeg(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicIncludePaths.AddRange(
+            new string[] {
 				// ... add public include paths required here ...
 			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
 				// ... add other private include paths required here ...
 			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
 				// ... add other public dependencies that you statically link with here ...
 			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
-			);
+            );
 
-        string LibrariesPath = Path.Combine(FfmpegPath, "lib");
+        string Platform = Target.Platform == UnrealTargetPlatform.Win64 ? "x64" : "win32";
+
+
+        string LibrariesPath = Path.Combine(FfmpegPath, "lib", Platform);
         string IncludePath = Path.Combine(FfmpegPath, "include");
-        string DllPath = Path.Combine(FfmpegPath, "dll");
+        string DllPath = Path.Combine(FfmpegPath, "bin", Platform);
 
-        PrivateIncludePaths.Add(IncludePath);
 
-        string[] dll = new string[]
+
+        string[] lib = new string[]
+        {
+                "avcodec.lib",
+                "avdevice.lib",
+                "avfilter.lib",
+                "avformat.lib",
+                "avutil.lib",
+                "postproc.lib",
+                "swresample.lib",
+                "swscale.lib"
+        };
+
+
+        foreach (string VARIABLE in lib)
+        {
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, VARIABLE));
+        }
+
+        string[] dlls = new string[]
         {
             "avcodec-58.dll",
             "avdevice-58.dll",
@@ -65,35 +105,22 @@ public class DigitFfmpeg : ModuleRules
             "swscale-5.dll"
         };
 
-        foreach (var VARIABLE in dll)
+
+        foreach (string VARIABLE in dlls)
         {
-            PublicDelayLoadDLLs.Add(Path.Combine(DllPath, VARIABLE));
+
+            //PublicDelayLoadDLLs.Add(VARIABLE);
+            //CopyToBinaries(Path.Combine(DllPath, VARIABLE), Target);
+            RuntimeDependencies.Add((Path.Combine(DllPath, VARIABLE)));
         }
 
-        string[] lib = new string[]
-        {
-            "avcodec.lib",
-            "avdevice.lib",
-            "avfilter.lib",
-            "avformat.lib",
-            "avutil.lib",
-            "postproc.lib",
-            "swresample.lib",
-            "swscale.lib"
-        };
-
-        foreach (string VARIABLE in lib)
-        {
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, VARIABLE));
-        }
-
-
+        PrivateIncludePaths.Add(IncludePath);
 
         DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
+            new string[]
+            {
 				// ... add any modules that your module loads dynamically here ...
 			}
-			);
-	}
+            );
+    }
 }
